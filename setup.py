d2746b88ebb407e5ababb28c31197f0f8643fad6
aalteirac <anthony@alteirac.com>
2022-11-25 10:09:59 +0100

    auto-release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setuptools.setup(
     name="streamlit-3d",
     #begin 
-    version="0.1.0", 
+    version="0.1.1", 
     #end
     author="Anthony Alteirac",
     author_email="anthony@alteirac.com",
